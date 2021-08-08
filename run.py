a81173969db60d67a9bcc0bd2ff4439f219764f8
reginaldj123 <gunt7701@gmail.com>
2021-08-08 18:22:26 +0000

    starting hangman

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-# python code goes here
+word = "Secret"
+
+allowed_errors = 7
+guesses = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+            else:
+                print("_", end=" ")
+        print("")
+        done = True
