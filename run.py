2f0bebdec7d83cb30b194bae5ce34e2fe6ba5815
reginaldj123 <gunt7701@gmail.com>
2021-08-10 22:25:04 +0000

    starting up game

--- run.py
+++ run.py
@@ -1,35 +1,73 @@
-word = "Secret"
+import string
 
-allowed_errors = 7
-guesses = []
-done = False
+LETTERS = list(string.ascii_lowercase)
 
-while not done:
-    for letter in word:
-        if letter.lower() in guesses:
-            print(letter, end=" ")
+"""def logic():
+
+
+    word = "Secret" 
+    allowed_errors = 7
+    guesses = []
+    done = False
+
+
+    while not done:
+        for letter in word:
+            if letter.lower() in guesses:
+                print(letter, end=" ")
 
             else:
-            
-             print("_", end=" ")
-        print("")
-    
 
-        guesses = input(f"Allowd errors left {allowed_errors}, Nex Guess: ")
+                print("_", end=" ")
+
+        print("")
+
+        guesses = input(f"Allowd errors left {allowed_errors}, Next Guess: ")
         guesses.append(guess.lower())
         if guess.lower() not in word.lower():
             allowed_errors -= 1
             if allowed_errors == 0:
                 break
 
- done = True
- for letter in word:
-     if letter.lower() not in guesses:
-         done = False
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
 
-         if done:
-             print(f"you found the word! it was {word}!")
-         else
-             print(f"Game over! The word was {word}!")
-   
-        
+            if done:
+                print(f"you found the word! it was {word}!")
+            else
+                print(f"Game over! The word was {word}!")
+    
+"""            
+def yes_no(question):
+    print(question)
+    print("yes = 1")
+    print("no = 2")
+    answer = input("enter your answer here \n").strip()
+    while answer not in ("1","2"):
+        print("please choose 1 for yes and 2 for no")
+        answer = input("enter your answer here \n").strip()
+    return answer
+
+
+def start_game():
+    word = "Secret" 
+    allowed_errors = 7
+    guesses = []
+    done = False
+
+
+def main():
+    print("Welcome")
+    answer = yes_no("do you want to play hang man ?")
+    if answer == 1 :
+        print("lets play")
+    else: 
+        print("goodbye")
+
+
+
+  
+
+main()
