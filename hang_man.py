f8350688c9cc834aac30a4623cea5eb0ae460102
reginaldj123 <gunt7701@gmail.com>
2021-08-17 19:01:39 +0000

    trying to re write hangman game as having problems with the old one that i cant seem to identify

---
+++ hang_man.py
@@ -0,0 +1,22 @@
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    gussed = False
+    gussed_letters = []
+    guessed_word = []
+    tries = 6
+
+def display_hangman = [
+    
+]
+
+
+
+
+
